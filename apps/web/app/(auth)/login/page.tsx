6c2f204b1ba3f0a82a053ce381ab65c524d28874
Puskar-Roy <puskarroy600@gmail.com>
2024-05-02 13:12:01 +0530

    feat : input styles

--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -6,11 +6,19 @@ export default function Page(): JSX.Element {
     <main className="w-[80%] mx-auto h-screen flex justify-center items-center">
       <div className="w-[90%] sm:w-[35%] mx-auto">
         <form className="flex flex-col gap-4">
-          <Input type="text" placeholder="Enter Email" className="bg-slate-200" />
-          <Input type="text" placeholder="Enter Password" className="bg-slate-200" />
+          <Input
+            type="text"
+            placeholder="Enter Email"
+            className="bg-slate-200  p-3 border-2 rounded-xl text-black focus:outline-none"
+          />
+          <Input
+            type="text"
+            placeholder="Enter Password"
+            className="bg-slate-200  p-3 border-2 rounded-xl text-black focus:outline-none"
+          />
           <Button
             appName="web"
-            className=" bg-rose-500 hover:bg-rose-400 text-white font-medium"
+            className=" bg-rose-500 hover:bg-rose-400 text-white font-medium  rounded-xl px-3 py-3"
             key="1"
           >
             Login
