d9302161db52d3b4bea146e6c8ac6d1b8ff5bc36
Puskar-Roy <puskarroy600@gmail.com>
2024-05-02 14:16:52 +0530

    feat : otp page

---
+++ apps/web/app/(auth)/login/[email]/page.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import { useParams } from "next/navigation";
+
+const page = () => {
+  const params = useParams<{ email: string }>();
+  const [otp, setOtp] = useState<string[]>(["", "", "", "", "", ""]);
+  const refs = useRef<Array<HTMLInputElement | null>>([]);
+
+  useEffect(() => {
+    refs.current = refs.current.slice(0, otp.length);
+  }, [otp]);
+
+  const handleChange = (index: number, value: string) => {
+    if (value.length > 1) return; // Limit input to single character
+    const newOtp = [...otp];
+    newOtp[index] = value;
+    setOtp(newOtp);
+
+    if (value === "" && index > 0 && refs.current[index - 1]) {
+      refs.current[index - 1]?.focus();
+    } else if (
+      index < otp.length - 1 &&
+      value.length > 0 &&
+      refs.current[index + 1]
+    ) {
+      refs.current[index + 1]?.focus();
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const otpValue = otp.join("");
+    console.log("OTP:", otpValue);
+  };
+  return (
+    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-12">
+      <div className="relative bg-white px-6 pt-10 pb-9 shadow-xl mx-auto w-full max-w-lg rounded-2xl">
+        <div className="mx-auto flex w-full max-w-md flex-col space-y-16">
+          <div className="flex flex-col items-center justify-center text-center space-y-2">
+            <div className="font-semibold text-3xl">
+              <p>Email Verification</p>
+            </div>
+            <div className="flex flex-row text-sm font-medium text-gray-400">
+              <p>We have sent a code to your email {params.email}</p>
+            </div>
+          </div>
+
+          <div>
+            <form onSubmit={handleSubmit}>
+              <div className="flex flex-col space-y-16">
+                <div className="flex flex-row items-center justify-between mx-auto w-full max-w-[27rem]">
+                  {otp.map((value, index) => (
+                    <div className="w-16 h-16" key={index}>
+                      <input
+                        ref={(el) => {
+                          if (el && refs.current[index] !== el) {
+                            refs.current[index] = el;
+                          }
+                        }}
+                        className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-600 text-lg bg-white focus:bg-gray-50 focus:ring-1 focus:border-none ring-rose-700"
+                        type="text"
+                        value={value}
+                        onChange={(e) => handleChange(index, e.target.value)}
+                        maxLength={1} // Limit input to single character
+                      />
+                    </div>
+                  ))}
+                </div>
+
+                <div className="flex flex-col space-y-5">
+                  <div>
+                    <button className="flex flex-row items-center justify-center text-center w-full border rounded-xl hover:bg-rose-400 outline-none py-5 bg-rose-500 border-none text-white shadow-sm text-base font-semibold">
+                      Verify Account
+                    </button>
+                  </div>
+
+                  <div className="flex flex-row items-center justify-center text-center text-sm font-medium space-x-1 text-gray-500">
+                    <p>Didn't receive code?</p>{" "}
+                    <a
+                      className="flex flex-row items-center text-rose-600"
+                      href="#"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Resend
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- apps/web/app/(auth)/login/page.tsx
+++ apps/web/app/(auth)/login/page.tsx
@@ -1,29 +1,55 @@
 import { Button } from "@repo/ui/button";
 import { Input } from "@repo/ui/input";
+import { Alata } from "next/font/google";
 
+const alata = Alata({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 export default function Page(): JSX.Element {
   return (
     <main className="w-[80%] mx-auto h-screen flex justify-center items-center">
-      <div className="w-[90%] sm:w-[35%] mx-auto">
-        <form className="flex flex-col gap-4">
-          <Input
-            type="text"
-            placeholder="Enter Email"
-            className="bg-slate-200  p-3 border-2 rounded-xl text-black focus:outline-none"
-          />
-          <Input
-            type="text"
-            placeholder="Enter Password"
-            className="bg-slate-200  p-3 border-2 rounded-xl text-black focus:outline-none"
-          />
-          <Button
-            appName="web"
-            className=" bg-rose-500 hover:bg-rose-400 text-white font-medium  rounded-xl px-3 py-3"
-            key="1"
+      <div className=" bg-white shadow-xl mx-auto w-[90%] rounded-2xl flex justify-center items-center h-[70%]">
+        <div className="w-[90%] sm:w-[35%] mx-auto flex gap-8 flex-col">
+          <div
+            className={`font-semibold text-4xl text-center ${alata.className}`}
           >
-            Login
-          </Button>
-        </form>
+            Login with <span className="text-rose-500">Email</span>
+          </div>
+          <form className="flex flex-col gap-2 rounded-xl">
+            <div className="flex flex-col gap-6 ">
+              <Input
+                type="text"
+                placeholder="Enter Email"
+                className="bg-slate-200  p-3 border-2 rounded-xl text-black focus:outline-none focus:ring-2 ring-rose-500"
+              />
+              <Input
+                type="text"
+                placeholder="Enter Password"
+                className="bg-slate-200  p-3 border-2 rounded-xl text-black focus:outline-none focus:ring-2 ring-rose-500"
+              />
+            </div>
+            <p className="text-end text-sm font-medium">
+              Forgot{" "}
+              <span className="text-rose-500 cursor-pointer ">Password ?</span>
+            </p>
+            <Button
+              appName="web"
+              className=" bg-rose-500 hover:bg-rose-400 text-white text-base font-semibold  rounded-xl px-3 py-4"
+              key="1"
+            >
+              Login
+            </Button>
+
+            <p className="text-center text-sm font-medium">
+              Did not have an account?{" "}
+              <span className="text-rose-500 cursor-pointer">
+                Register
+              </span>
+            </p>
+          </form>
+        </div>
       </div>
     </main>
   );
