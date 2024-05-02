a4c6c25291e85200eb2851515a2a2ad8d000d134
Puskar-Roy <puskarroy600@gmail.com>
2024-05-02 12:50:43 +0530

    fix : login form

---
+++ apps/web/app/(main)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Page(): JSX.Element {
+  return (
+    <main className="w-[80%] mx-auto h-screen flex justify-center items-center">
+            Hello User 👋
+    </main>
+  );
+}
