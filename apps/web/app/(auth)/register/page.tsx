3394c6e3e1c8de9b014a04d48f8adfe6918256b3
Puskar-Roy <puskarroy600@gmail.com>
2024-05-02 14:22:50 +0530

    feat : register page

--- apps/web/app/(auth)/register/page.tsx
+++ apps/web/app/(auth)/register/page.tsx
@@ -1,15 +1,16 @@
-"use client";
+"use client"
 import { Button } from "@repo/ui/button";
 import { Input } from "@repo/ui/input";
 import { Alata } from "next/font/google";
+// import { useRouter } from "next/navigation";
 import Link from "next/link";
-
 const alata = Alata({
   weight: "400",
   subsets: ["latin"],
   display: "swap",
 });
 export default function Page(): JSX.Element {
+    
   return (
     <main className="w-[80%] mx-auto h-screen flex justify-center items-center">
       <div className=" bg-white shadow-xl mx-auto w-[90%] rounded-2xl flex justify-center items-center h-[70%]">
@@ -17,10 +18,15 @@ export default function Page(): JSX.Element {
           <div
             className={`font-semibold text-4xl text-center ${alata.className}`}
           >
-            Login with <span className="text-rose-500">Email</span>
+            Register with <span className="text-rose-500">Email</span>
           </div>
-          <form className="flex flex-col gap-2 rounded-xl">
-            <div className="flex flex-col gap-6 ">
+          <form className="flex flex-col gap-4 rounded-xl">
+            <div className="flex flex-col gap-4 ">
+              <Input
+                type="text"
+                placeholder="Enter Name"
+                className="bg-slate-200  p-3 border-2 rounded-xl text-black focus:outline-none focus:ring-2 ring-rose-500"
+              />
               <Input
                 type="text"
                 placeholder="Enter Email"
@@ -32,22 +38,19 @@ export default function Page(): JSX.Element {
                 className="bg-slate-200  p-3 border-2 rounded-xl text-black focus:outline-none focus:ring-2 ring-rose-500"
               />
             </div>
-            <p className="text-end text-sm font-medium">
-              Forgot{" "}
-              <span className="text-rose-500 cursor-pointer ">Password ?</span>
-            </p>
+
             <Button
               appName="web"
               className=" bg-rose-500 hover:bg-rose-400 text-white text-base font-semibold  rounded-xl px-3 py-4"
               key="1"
             >
-              Login
+              Create Account
             </Button>
 
             <p className="text-center text-sm font-medium">
-              Did not have an account?{" "}
+              Already have an account?{" "}
               <span className="text-rose-500 cursor-pointer">
-                <Link href="/register">Register</Link>
+                <Link href='/login'>Login</Link>
               </span>
             </p>
           </form>
