8daef05dabb6e12beceb32ae80fd3874c334c23b
Puskar-Roy <puskarroy600@gmail.com>
2024-05-02 12:44:37 +0530

    feat : login form

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,22 @@
 import { Button } from "@repo/ui/button";
+import { Input } from "@repo/ui/input";
+
 export default function Page(): JSX.Element {
   return (
-    <main className="text-4xl text-rose-500">
-      Hello
-      <Button className="text-2xl p-3 bg-rose-500 text-white rounded-xl" appName="web">
-        Hello
-      </Button>
+    <main className="w-[80%] mx-auto h-screen flex justify-center items-center">
+      <div className="w-[90%] sm:w-[35%] mx-auto">
+        <form className="flex flex-col gap-4">
+          <Input placeholder="Enter Email" className="bg-slate-200" />
+          <Input placeholder="Enter Password" className="bg-slate-200" />
+          <Button
+            appName="web"
+            className=" bg-rose-500 hover:bg-rose-400 text-white font-medium"
+            key="1"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
     </main>
   );
 }
