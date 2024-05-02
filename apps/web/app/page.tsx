f070fd72688e850fef927fb6b87e2a6916d3881a
Puskar-Roy <puskarroy600@gmail.com>
2024-05-02 12:20:06 +0530

    feat : ui package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,11 @@
-
+import { Button } from "@repo/ui/button";
 export default function Page(): JSX.Element {
   return (
     <main className="text-4xl text-rose-500">
-     Hello
+      Hello
+      <Button className="text-2xl p-3 bg-rose-500 text-white rounded-xl" appName="web">
+        Hello
+      </Button>
     </main>
   );
 }
