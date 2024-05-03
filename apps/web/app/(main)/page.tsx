be03796b3d990eafba00f5672fe8b5367abd15c5
Puskar-Roy <puskarroy600@gmail.com>
2024-05-03 23:06:37 +0530

    feat : device info in ui

--- apps/web/app/(main)/page.tsx
+++ apps/web/app/(main)/page.tsx
@@ -5,6 +5,15 @@ import { useRouter } from "next/navigation";
 import socketIOClient, { Socket } from "socket.io-client";
 import { LoginHistory } from "../../interfaces";
 import axios from "axios";
+import { CgBrowser } from "react-icons/cg";
+import { MdDevices, MdOutlineAccessTimeFilled } from "react-icons/md";
+import { Alata } from "next/font/google";
+import { GoDotFill } from "react-icons/go";
+const alata = Alata({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Page(): JSX.Element {
   const { state } = useAuthContext();
@@ -52,21 +61,62 @@ export default function Page(): JSX.Element {
   }
 
   return (
-    <main className="w-[80%] mx-auto h-screen flex justify-center items-center flex-col">
-      <div>Hello {state.user?.name} </div>
-      <div>Total Active User - {activeUsers}</div>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <ul>
-          {loginHistory.map((history) => (
-            <li key={history._id}>
-              {new Date(history.timestamp).toLocaleString()} -{" "}
-              {history.deviceInfo} ({history.os})
-            </li>
-          ))}
-        </ul>
-      )}
+    <main className="w-[80%] mx-auto min-h-screen flex  items-center flex-col">
+      <div>
+        <div className="text-3xl">
+          Hello{" "}
+          <span className="text-rose-500 font-semibold">
+            {state.user?.name}
+          </span>{" "}
+          🖐️
+        </div>
+      </div>
+
+      <div className="flex bg-green-100 p-3 rounded-xl justify-center items-center gap-1 flex-row absolute right-4 sm:right-8 bottom-10 sm:bottom-auto sm:top-5 z-20">
+        <GoDotFill className="text-green-500 text-xl" />
+        Active Users -
+        <span className="text-rose-500 font-semibold text-xl">
+          {activeUsers}
+        </span>
+      </div>
+      <div className="w-[80%] mx-auto flex justify-center items-center flex-col gap-6">
+        <div
+          className={`font-semibold text-4xl text-center ${alata.className}`}
+        >
+          Login <span className="text-rose-500">History</span>
+        </div>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <ul className="w-[80%] flex justify-center items-center flex-wrap gap-3">
+            {loginHistory.map((history) => (
+              <li
+                className="w-[15rem] h-[7rem]  p-[50px] shadow-2xl justify-center flex items-center flex-col rounded-xl"
+                key={history._id}
+              >
+                <div className="flex justify-between items-center min-w-[13rem]">
+                  <CgBrowser className="text-3xl" />{" "}
+                  <span className="text-lg text-rose-500 font-semibold">
+                    {history.deviceInfo}
+                  </span>
+                </div>
+                <div className="flex justify-between items-center min-w-[13rem]">
+                  <MdDevices className="text-3xl" />
+                  <span className="text-lg text-rose-500 font-semibold">
+                    {history.os}
+                  </span>
+                </div>
+                <div className="flex justify-between items-center min-w-[13rem]">
+                  <MdOutlineAccessTimeFilled className="text-3xl" />
+                  <span className="text-sm text-rose-500 font-semibold">
+                    {new Date(history.timestamp).toLocaleString()}
+                  </span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </main>
   );
 }
