2415b6c71a4df8a62746e8eb4d5321668ff25994
Puskar-Roy <puskarroy600@gmail.com>
2024-05-03 10:19:09 +0530

    feat : forgot password

--- apps/web/app/(auth)/forgot-password/[email]/page.tsx
+++ apps/web/app/(auth)/forgot-password/[email]/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "next/navigation";
+import { Input } from "@repo/ui/input";
 
 const page = () => {
   const params = useParams<{ email: string }>();
@@ -13,7 +14,7 @@ const page = () => {
   }, [otp]);
 
   const handleChange = (index: number, value: string) => {
-    if (value.length > 1) return; 
+    if (value.length > 1) return;
     const newOtp = [...otp];
     newOtp[index] = value;
     setOtp(newOtp);
@@ -37,10 +38,10 @@ const page = () => {
   return (
     <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-12">
       <div className="relative bg-white px-6 pt-10 pb-9 shadow-xl mx-auto w-full max-w-lg rounded-2xl">
-        <div className="mx-auto flex w-full max-w-md flex-col space-y-16">
+        <div className="mx-auto flex w-full max-w-md flex-col space-y-10">
           <div className="flex flex-col items-center justify-center text-center space-y-2">
             <div className="font-semibold text-3xl">
-              <p>Email Verification</p>
+              <p>Reset Your Password</p>
             </div>
             <div className="flex flex-row text-sm font-medium text-gray-400">
               <p>We have sent a code to your email {params.email}</p>
@@ -49,30 +50,42 @@ const page = () => {
 
           <div>
             <form onSubmit={handleSubmit}>
-              <div className="flex flex-col space-y-16">
-                <div className="flex flex-row items-center justify-between mx-auto w-full max-w-[27rem]">
-                  {otp.map((value, index) => (
-                    <div className="w-16 h-16" key={index}>
-                      <input
-                        ref={(el) => {
-                          if (el && refs.current[index] !== el) {
-                            refs.current[index] = el;
-                          }
-                        }}
-                        className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-600 text-lg bg-white focus:bg-gray-50 focus:ring-1 focus:border-none ring-rose-700"
-                        type="text"
-                        value={value}
-                        onChange={(e) => handleChange(index, e.target.value)}
-                        maxLength={1} 
-                      />
-                    </div>
-                  ))}
+              <div className="flex flex-col space-y-6">
+                <div className="flex justify-center items-center flex-col gap-5">
+                  <div className="flex flex-row items-center justify-between mx-auto w-full max-w-[27rem]">
+                    {otp.map((value, index) => (
+                      <div className="w-16 h-16" key={index}>
+                        <input
+                          ref={(el) => {
+                            if (el && refs.current[index] !== el) {
+                              refs.current[index] = el;
+                            }
+                          }}
+                          className="w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-600 text-lg bg-white focus:bg-gray-50 focus:ring-1 focus:border-none ring-rose-700"
+                          type="text"
+                          value={value}
+                          onChange={(e) => handleChange(index, e.target.value)}
+                          maxLength={1}
+                        />
+                      </div>
+                    ))}
+                  </div>
+                  <Input
+                    type="text"
+                    placeholder="Enter New Password"
+                    className="bg-slate-200  w-full border-2 rounded-xl text-black focus:outline-none focus:ring-2 ring-rose-500"
+                  />
+                  <Input
+                    type="text"
+                    placeholder="Confirm New Password"
+                    className="bg-slate-200  w-full border-2 rounded-xl text-black focus:outline-none focus:ring-2 ring-rose-500"
+                  />
                 </div>
 
                 <div className="flex flex-col space-y-5">
                   <div>
                     <button className="flex flex-row items-center justify-center text-center w-full border rounded-xl hover:bg-rose-400 outline-none py-5 bg-rose-500 border-none text-white shadow-sm text-base font-semibold">
-                      Verify Account
+                      Change Password
                     </button>
                   </div>
 
--- apps/web/app/(auth)/forgot-password/page.tsx
+++ apps/web/app/(auth)/forgot-password/page.tsx
@@ -17,33 +17,22 @@ export default function Page(): JSX.Element {
           <div
             className={`font-semibold text-4xl text-center ${alata.className}`}
           >
-            Login with <span className="text-rose-500">Email</span>
+            Forgot <span className="text-rose-500">Password</span>
           </div>
-          <form className="flex flex-col gap-2 rounded-xl">
+          <form className="flex flex-col gap-4 rounded-xl">
             <div className="flex flex-col gap-6 ">
               <Input
                 type="text"
                 placeholder="Enter Email"
                 className="bg-slate-200  p-3 border-2 rounded-xl text-black focus:outline-none focus:ring-2 ring-rose-500"
               />
-              <Input
-                type="text"
-                placeholder="Enter Password"
-                className="bg-slate-200  p-3 border-2 rounded-xl text-black focus:outline-none focus:ring-2 ring-rose-500"
-              />
             </div>
-            <p className="text-end text-sm font-medium">
-              Forgot{" "}
-              <span className="text-rose-500 cursor-pointer ">
-                <Link href='forgot-password'>Password ?</Link>
-              </span>
-            </p>
             <Button
               appName="web"
               className=" bg-rose-500 hover:bg-rose-400 text-white text-base font-semibold  rounded-xl px-3 py-4"
               key="1"
             >
-              Login
+              Find Account
             </Button>
 
             <p className="text-center text-sm font-medium">
