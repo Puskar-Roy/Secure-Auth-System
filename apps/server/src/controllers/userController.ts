9b522a242eb7b0407105c5c7e9717f39bca8eacc
Puskar-Roy <puskarroy600@gmail.com>
2024-05-05 10:34:17 +0530

    feat : user controllers

---
+++ apps/server/src/controllers/userController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import asyncHandler from "../util/catchAsync";
+import UserModel from "../models/userSchema";
+import VerifyModel from "../models/tokenSchema";
+import LoginHistoryModel from "../models/loginHistorySchema";
+
+export const getUser = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params;
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json({ ...user.toJSON(), password: undefined });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+export const getAllUsers = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const users = await UserModel.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
